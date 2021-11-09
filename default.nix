d056819720da30e7c49107e7ee4808e9b820a3b8
Jorge Acereda <jacereda@gmail.com>
2021-11-09 21:22:22 +0100

    Switch to my cosmopolitan repo

--- default.nix
+++ default.nix
@@ -1,19 +1,20 @@
-{ pkgs ? import (fetchTarball {
-  name = "nixpkgs";
-  url = "https://github.com/NixOS/nixpkgs/archive/fa90ef8.tar.gz";
-  sha256 = "0nfazdiga80nks55jd7jxg0vxv0v7pispizx9v8r0l2ik9qgx70b";
-}) {}
+{ pkgs ? import <nixpkgs> {}
 }:
 with pkgs;
 {
   shellEnv =
     let
-#      cosmopolitan-local = callPackage ../cosmopolitan/default.nix {};
+      cosmopolitan-src = fetchTarball {
+        name = "cosmopolitan";
+        url = "https://github.com/jacereda/cosmopolitan/archive/7df37b5.tar.gz";
+        sha256 = "1pl4d5w2kmn08ysmf8xhvg5n7smal253jw1anjnwncf1na1gawrg";
+      };
+      cosmopolitan = callPackage cosmopolitan-src {};
       mylibs = with xorg; [ glibc libGL libX11 libXcursor libXrender libxkbcommon ];
     in
       stdenv.mkDerivation {
         name = "shell-environment";
-        nativeBuildInputs = with pkgs; [ pkg-config wayland-protocols gdb clang-tools cosmopolitan ];
+        nativeBuildInputs = with pkgs; [ gdb clang-tools cosmopolitan ];
         buildInputs = mylibs;
         LD_LIBRARY_PATH = lib.makeLibraryPath mylibs;
       };
